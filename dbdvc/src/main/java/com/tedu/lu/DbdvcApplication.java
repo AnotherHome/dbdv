50bb08f5328713aea05b18885305d52e9f7cfb91
Administrator <Administrator@tarena>
2019-12-05 19:28:38 +0800

    1

---
+++ dbdvc/src/main/java/com/tedu/lu/DbdvcApplication.java
@@ -0,0 +1,13 @@
+package com.tedu.lu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbdvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbdvcApplication.class, args);
+	}
+
+}
---
+++ dbdvc/src/test/java/com/tedu/lu/DbdvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedu.lu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbdvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
